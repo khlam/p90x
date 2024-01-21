a18465fc70b6715ce4ff6944bb603af2b21f02ce
khlam <4841220+khlam@users.noreply.github.com>
2024-01-21 13:01:06 -0800

    format

--- read_latest_csv.py
+++ read_latest_csv.py
@@ -36,10 +36,10 @@ def csv_to_markdown_table_and_totals(file_name):
             week_number = date_obj.isocalendar()[1]
             day = date_obj.day
 
-            # Apply italics for even weeks, normal for odd weeks
+            # Apply bold for even weeks, normal for odd weeks
             if week_number % 2 == 0:
                 # Even weeks
-                day_str = f"*{day}*"  # Italics
+                day_str = f"**{day}**"  # Bold
             else:
                 # Odd weeks
                 day_str = str(day)  # Normal text
