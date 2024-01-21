a35828e8e4645d39aa8d757da69fcb15d3529f0d
khlam <4841220+khlam@users.noreply.github.com>
2024-01-20 23:45:28 -0800

    test

--- read_latest_csv.py
+++ read_latest_csv.py
@@ -12,41 +12,41 @@ def extract_month_and_year_from_filename(filename):
     name, ext = os.path.splitext(base)
     try:
         date = datetime.strptime(name, '%Y-%m')
-        return date.strftime('%B %Y')  # Format changed to include the year
+        return date.strftime('%B %Y')
     except ValueError:
         return "Unknown Date"
 
-def csv_to_markdown_table_and_monthly_earnings(file_name):
-    monthly_earnings = {}
+def csv_to_markdown_table_and_totals(file_name):
+    monthly_totals = {}
     table = ""
 
     with open(file_name, mode='r', encoding='utf-8') as file:
         reader = csv.reader(file)
         headers = next(reader)
         for header in headers[1:]:
-            monthly_earnings[header] = 0
+            monthly_totals[header] = 0
 
-        # Markdown table headers
         table += "| " + " | ".join(headers) + " |\n"
         table += "| " + " | ".join(['---'] * len(headers)) + " |\n"
 
         for row in reader:
             table += "| " + " | ".join(row) + " |\n"
-            for i, earning in enumerate(row[1:], start=1):
+            for i, value in enumerate(row[1:], start=1):
                 try:
-                    monthly_earnings[headers[i]] += float(earning)
+                    monthly_totals[headers[i]] += float(value)
                 except ValueError:
-                    pass  # Ignore if conversion to float fails or field is empty
+                    pass
 
-    return table, monthly_earnings
+    return table, monthly_totals
 
-def update_readme(csv_file, month, data_table, monthly_earnings):
+def update_readme(csv_file, month, data_table, monthly_totals):
     with open('README.md', 'w', encoding='utf-8') as readme:
         readme.write(f"## [{month}]({csv_file})\n\n")
 
-        readme.write("| --- | ---: |\n")  # Align numbers to the right
-        for client, earning in monthly_earnings.items():
-            readme.write(f"| {client} | {earning:.2f} |\n")
+        readme.write("| Identifier | Total |\n")
+        readme.write("| --- | ---: |\n")
+        for identifier, total in monthly_totals.items():
+            readme.write(f"| {identifier} | {total:.2f} |\n")
 
         readme.write("\n")
         readme.write(data_table)
@@ -54,5 +54,5 @@ def update_readme(csv_file, month, data_table, monthly_earnings):
 if __name__ == "__main__":
     latest_csv = find_latest_csv()
     month = extract_month_and_year_from_filename(latest_csv)
-    markdown_table, monthly_earnings = csv_to_markdown_table_and_monthly_earnings(latest_csv)
-    update_readme(latest_csv, month, markdown_table, monthly_earnings)
+    markdown_table, monthly_totals = csv_to_markdown_table_and_totals(latest_csv)
+    update_readme(latest_csv, month, markdown_table, monthly_totals)
